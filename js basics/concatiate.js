26f0c381d88b9edd80d41dda2e021f752133d2e5
learnenggvgd <learnenggvgd@gmail.com>
2022-05-30 11:51:31 +0530

    js first commit

---
+++ js basics/concatiate.js
@@ -0,0 +1,5 @@
+var cmpName = "Luminar";
+var place = "kakkanad";
+console.log("our company is"+cmpName+","+place);
+
+console.log(`Our company is ${cmpName}${place}`);
---
+++ js basics/js.js
@@ -0,0 +1,32 @@
+
+
+var n = 5 ;
+var i = 0;
+console.log("number = "+ n);
+console.log("before loop = "+ i);
+for(var i=0;i<=n;i++)
+{
+console.log("multiplication = "+ i*n);
+if(i==n){
+    console.log("inner loop = "+ i);
+    }
+
+}
+console.log("before loop = "+ i);
+
+// 2nd
+var x = 5 ;
+var y = 0;
+console.log("number = "+ n);
+console.log("before loop = "+ i);
+for(let y=0;y<=x;y++)
+{
+console.log("multiplication = "+ y*x);
+if(i=0){
+    console.log("inner loop = "+ y);
+    }
+
+}
+console.log("before loop = "+ y);
+
+
