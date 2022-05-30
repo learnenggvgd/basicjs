52d37ca9a621ff2c55001da0a7897a776311b37c
learnenggvgd <106076843+learnenggvgd@users.noreply.github.com>
2022-05-30 12:06:45 +0530

    Add files via upload

---
+++ js basics/operator.js
@@ -0,0 +1,16 @@
+var a = 100;
+var b= 3;
+
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+var g = a % b;
+var h = a ** 2;
+
+console.log("c="+c);
+console.log("d="+d);
+console.log("e="+e);
+console.log("f="+f);
+console.log("g="+g);
+console.log("h="+h);
