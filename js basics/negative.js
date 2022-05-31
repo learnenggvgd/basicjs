bf60add5d25bc59b7239beff5511fd1e8b8a0fd9
learnenggvgd <106076843+learnenggvgd@users.noreply.github.com>
2022-05-31 15:20:54 +0530

    Add files via upload

---
+++ js basics/negative.js
@@ -0,0 +1,21 @@
+var num1 = -5;
+var num2 = +5;
+console.log("--------------------------------------");
+console.log("Num1 age ="+num1);
+console.log("Num2 age ="+num2);
+console.log("--------------------------------------");
+
+if(num1>0){
+    console.log("Num1 is Positive : "+num1);
+}
+else{
+    console.log("Num1 Is negative : " +num2); 
+}
+
+
+if(num2>0){
+    console.log("Num1 is Positive : "+num2);
+}
+else{
+    console.log("Num1 Is negative : " +num2); 
+}
---
+++ js basics/voter.js
@@ -0,0 +1,28 @@
+var aswin = 25;
+var manu = 10;
+console.log("--------------------------------------");
+console.log("Aswin age ="+aswin);
+console.log("Manu age ="+manu);
+console.log("--------------------------------------");
+
+if(aswin>=18){
+    console.log("Aswin Eligible for Voting");
+}
+else{
+    console.log("Aswin Not Eligible for Voting"); 
+}
+
+
+if(manu>=18){
+    console.log("Manu Eligible for Voting");
+}
+else{
+    console.log("Manu not Eligible for Voting"); 
+}
+console.log("-----------------Aswin and manu---------------------");
+if(aswin>=18 && manu>=18){
+    console.log("Aswin and Manu Eligible for Voting");
+}
+else{
+    console.log("Aswin and Manu both are Eligible for Voting"); 
+}
